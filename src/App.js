2517784b9772ad1ba8048caf6253d914e0f9d5c3
Sharad Patel <sharad.patels03.com>
2023-10-27 11:12:10 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-
+import React from 'react';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import './App.css';
 import Information from './Components/Information/Information';
 import Login from './Components/Login/Login';
 import FormP from './Pages/FormP/FormP';
 import SubmissionP from './Pages/SubmissionP/Submission';
 
+
+const router=createBrowserRouter([{
+  path:'/',
+  element:<FormP/>
+},
+{
+  path:'/upsc',
+  element:<SubmissionP/>
+}])
+
 function App() {
   return (
-    <>
-    <FormP/>
-         </>
+    <RouterProvider router={router}/>
   );
 }
 
